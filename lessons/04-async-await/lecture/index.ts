2c94bd046d4c102b0b4c296b063848d4f8d01f1a
Brad Westfall <brad@builtwithweb.com>
2024-06-20 08:59:54 -0400

    better starting point

--- lessons/04-async-await/lecture/index.ts
+++ lessons/04-async-await/lecture/index.ts
@@ -1,18 +1,45 @@
-import { User, createAccount, addAccountUser } from './users'
+// import { User, createAccount, addAccountUser } from './users'
 
-function signup(user: User) {
-  return createAccount()
-    .then((account) => {
-      return addAccountUser(account.accountId, user)
-    })
-    .then((user) => {
-      // emailUser(user)
-      // logNewUserStats(account.accountId)
-    })
+/****************************************
+  Part 1
+*****************************************/
+
+function getVehicle(url: string) {
+  return fetch(url).then((response) => response.json())
 }
 
-signup({ name: 'brad' }).then(() => {
-  console.log('✅ User Added')
-})
+function getPersonVehicles(id: number): Promise<string[]> {
+  return fetch(`https://swapi.dev/api/people/${id}`)
+    .then((response) => response.json() as Record<string, any>)
+    .then((data) => data.vehicles)
+}
 
-// Remember "top-level" await
+getPersonVehicles(1)
+  .then((vehicles) => {
+    const p = vehicles.map((url) => getVehicle(url))
+    return Promise.all(p)
+  })
+  .then((allVehicles) => {
+    console.log(allVehicles)
+  })
+
+/****************************************
+  Part 2
+*****************************************/
+
+// function signup(user: User) {
+//   return createAccount()
+//     .then((account) => {
+//       return addAccountUser(account.accountId, user)
+//     })
+//     .then((user) => {
+//       // emailUser(user)
+//       // logNewUserStats(account.accountId)
+//     })
+// }
+
+// signup({ name: 'brad' }).then(() => {
+//   console.log('✅ User Added')
+// })
+
+// // Remember "top-level" await
